e9960191f820de2d282c834487c6f65ab14bd35f
gong jinye <kth_gjy0508082021@163.com>
2026-01-21 20:06:36 +0100

    update task5

--- SiLagoNN/phy/scr/read_design.tcl
+++ SiLagoNN/phy/scr/read_design.tcl
@@ -1,11 +1,10 @@
-#1. source global variables
-#2. source design variables
-
+# Read design: MMMC, LEF, netlist, and initialize
 set_multi_cpu_usage -local_cpu ${NUM_CPUS} -cpu_per_remote_host 1 -remote_host 0 -keep_license true
 set_distributed_hosts -local
-#3. set vdd net
-#4. set vss net
-#5. read mmmc file
-#6. read lef 
-#7. read logic synthesis netlist
+set_db init_power_nets {VDD}
+set_db init_ground_nets {VSS}
+set_db init_design_uniquify true
+read_mmmc ${MMMC_FILE}
+read_physical -lef ${LEF_FILE}
+read_netlist ${NETLIST_FILE}
 init_design
